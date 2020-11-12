28562ea70a850fd4171a1885b835c50e4ab8049b
spl211 <refaelfarjun5@gmail.com>
2020-11-12 04:22:11 +0200

    after making Cmake

---
+++ cmake-build-debug/CMakeFiles/SPL.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPL.dir/Agent.cpp.o"
+  "CMakeFiles/SPL.dir/Graph.cpp.o"
+  "CMakeFiles/SPL.dir/Session.cpp.o"
+  "CMakeFiles/SPL.dir/Tree.cpp.o"
+  "CMakeFiles/SPL.dir/main.cpp.o"
+  "SPL"
+  "SPL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
